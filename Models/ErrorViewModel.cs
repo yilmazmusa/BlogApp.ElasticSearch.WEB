7bd9b9ec456272aee5586cb5710a6e5ff6a6ac81
MUSA YILMAZ <yilmazmusa66@gmail.com>
2023-11-10 17:10:48 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogApp.ElasticSearch.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
